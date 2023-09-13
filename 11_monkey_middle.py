1995418439278add022bd7a89348bbfdcdfa19c1
Kevin Buchik <kdbuchik@gmail.com>
2023-09-12 23:51:46 -0700

    Completed Monkey class for Day 11 solution

--- 11_monkey_middle.py
+++ 11_monkey_middle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 ## Advent of Code 2022
-## Day 11 - Supply Stacks
+## Day 11 - Monkey in the Middle
 ## https://adventofcode.com/2022/day/11
 
 import os
@@ -18,39 +18,33 @@ class Monkey:
         self.falseTarget = falseTarget
     
     @staticmethod
-    def makeMonkey(descStr):
+    def makeMonkey(descLines):
         sItems = list()
-        sOper = None
+        sOper = "old"
         sTest = 1
         sTrue = -1
         sFalse = -1
-        if debug:
-            print("Building monkey with the following descString:")
-            print(descStr)
-        for line in descStr:
+        for line in descLines:
             line = line.strip()
+            if debug:
+                print("\t" + line)
             if line.startswith("Starting items"):
-                iList = line.split(" ")[2:]
+                iList = line[16:].split(", ")
                 for i in iList:
-                    if i.endsWith(","):
-                        i = i[:-1]
                     sItems.append(int(i))
             elif line.startswith("Operation"):
-                iOps = line.split(" ")[1:]
-                sOper = (lambda n: n)
-                # I have no idea how to implement this lol
+                sOper = line[17:]
             elif line.startswith("Test"):
-                sTest = int(line.split(" ")[-1:])
+                sTest = int(line.split(" ")[-1])
             elif line.startswith("If true"):
-                sTrue = int(line.split(" ")[-1:])
+                sTrue = int(line.split(" ")[-1])
             elif line.startswith("If false"):
-                sFalse = int(line.split(" ")[-1:])
+                sFalse = int(line.split(" ")[-1])
         return Monkey(sItems, sOper, sTest, sTrue, sFalse)
         
     def __str__(self):
-        descStr = "\tStarting items: " + ", ".join(self.items)
-        descStr = descStr[:-2] + "\n"
-        descStr += "\tOperation: {0}\n".format(str(self.oper))
+        descStr = "\tStarting items: {0}\n".format(", ".join(map((lambda i: str(i)), self.items)))
+        descStr += "\tOperation: new = {0}\n".format(self.oper)
         descStr += f"\tTest: divisible by {self.testValue}\n"
         descStr += f"\t\tIf true: throw to monkey {self.trueTarget}\n"
         descStr += f"\t\tIf false: throw to monkey {self.falseTarget}\n"
@@ -59,8 +53,8 @@ class Monkey:
     def giveItem(i):
         items.append(i)
     
-    def doOper(n):
-        return oper(n)
+    def doOper(old):
+        return eval(oper)
     
     def modTest(n):
         return (n % testValue) == 0
@@ -72,29 +66,28 @@ part1sum = 0
 with open("input11.txt") as f:
     currMonkey = 0
     buildingMonkey = False
-    monkeyDesc = ""
+    monkeyDesc = list()
     for line in f:
         line = line.rstrip()
         if buildingMonkey:
             if len(line) == 0 or line.startswith("Monkey"):
                 monkeys[currMonkey] = Monkey.makeMonkey(monkeyDesc)
                 buildingMonkey = False
-                monkeyDesc = ""
+                monkeyDesc = list()
             else:
-                monkeyDesc += line
+                monkeyDesc.append(line)
         if line.startswith("Monkey"):
             buildingMonkey = True
             currMonkey = int(line.split(" ")[1][:-1])
-            print("Building monkey " + str(currMonkey))
-
-exit(0)
+    if buildingMonkey:
+        monkeys[currMonkey] = Monkey.makeMonkey(monkeyDesc)
 
 for key, value in monkeys.items():
     print(f"Monkey {key}:")
     print(str(value))
 
 print("Part One:")
-#print(tops1 + "\n")
+
 #print("Part Two:")
-#print(tops2 + "\n")
+
 exit(0)
